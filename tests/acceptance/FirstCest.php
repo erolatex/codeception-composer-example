deb91790acc54f14a245bad93da102adcba3ee2a
Maksim Rabinovich <redfox@wisebits.com>
2020-02-04 20:23:18 +0200

    Add Allure annotations

--- tests/acceptance/FirstCest.php
+++ tests/acceptance/FirstCest.php
@@ -1,9 +1,44 @@
 <?php
-class FirstCest
+
+use Codeception\Example;
+use Yandex\Allure\Adapter\Model\TestCase;
+use Yandex\Allure\Adapter\Annotation\Title;
+use Yandex\Allure\Adapter\Annotation\Description;
+use Yandex\Allure\Adapter\Annotation\TestCaseId;
+use Yandex\Allure\Adapter\Annotation\Issues;
+
+/**
+ * Class FirstCest
+ * Tests links
+ */
+class PageCest extends TestCase
 {
-    public function frontpageWorks(AcceptanceTester $I)
+    /**
+     * @Test
+     * @Title("Test link title")
+     * @Issues("SSS-1111")
+     * @TestCaseId("23")
+     * @Description("Test link test desc")
+     * @dataProvider pageProvider
+     * @param AcceptanceTester $I
+     * @param Example $example
+     */
+    public function staticPages(AcceptanceTester $I, Example $example)
     {
-        $I->amOnPage('/');
-        $I->see('Найти');
+        $I->amOnPage($example['url']);
+        $I->see($example['title']);
+    }
+
+    /**
+     * @return array
+     */
+    protected function pageProvider()
+    {
+        return [
+            ['url' => "/", 'title' => "Website"],
+            ['url' => "/info", 'title' => "Info"],
+            ['url' => "/about-us", 'title' => "About Us"],
+            ['url' => "/contact-us", 'title' => "Contact Us"]
+        ];
     }
 }
